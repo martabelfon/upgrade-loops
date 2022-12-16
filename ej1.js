acce14b7a8bd4b650596dc3e603972c962c0a78a
martabelfon <martabelfon@gmail.com>
2022-12-16 13:07:16 +0100

    iteracion1

--- ej1.js
+++ ej1.js
@@ -4,3 +4,10 @@
 //la palabra "Camiseta". Usa la función .***includes*** de javascript.
 
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+//creamos el for of para que me muestre todos los productos que contiene nuestra array
+for (const product of products) {
+    //si el producto que estoy sacando contiene "camiseta" lo muestro, si no, paso de él
+    if (product.includes("Camiseta"))
+    console.log(product);
+}
