c0b185bb3bcd8819dbc42286923319fc394e63e4
martabelfon <martabelfon@gmail.com>
2022-12-16 12:49:55 +0100

    Ejercicios JS Loops

---
+++ ej6.js
@@ -0,0 +1,12 @@
+// **Iteración #6: Mixed For e includes SI**
+
+// Usa un bucle **for** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. 
+// Recuerda que puedes usar la función ***.includes()*** para comprobarlo.Puedes usar este array:
+
+const toys = [
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
