0a2c3b1aba403d9cd73692fc83f2c4c82b74633f
martabelfon <martabelfon@gmail.com>
2023-01-12 18:28:36 +0100

    Iteracion 5 OK

--- ej5.js
+++ ej5.js
@@ -7,13 +7,16 @@
 const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
 
 
-for (const place of placesToTravel) {
+for (let i = 0; i < placesToTravel.length; i++) {
     // console.log(place.id);
-       if (place.id == 11 || place.id == 40) {
-        place.splice(0,0);
+       if (placesToTravel[i].id == 11 || placesToTravel[i].id == 40) {
+        placesToTravel.splice(i,1);
+        i--;
        }
-       console.log(place);
+       
 }
+
+console.log(placesToTravel);
 
 
 
